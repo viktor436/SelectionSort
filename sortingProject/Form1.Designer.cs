399883a87552e338bc45cd0b7b1c6b7de66ce9be
viktor436 <eeeevv54@gmail.com>
2023-04-06 10:11:02 +0300

    Add project files.

---
+++ sortingProject/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿
+namespace sortingProject
+{
+    partial class FormSort
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.panelSorting = new System.Windows.Forms.Panel();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox0 = new System.Windows.Forms.TextBox();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.comboBoxSpeed = new System.Windows.Forms.ComboBox();
+            this.labelSpeed = new System.Windows.Forms.Label();
+            this.panelSorting.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(418, 12);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 34);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // panelSorting
+            // 
+            this.panelSorting.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelSorting.Controls.Add(this.textBox9);
+            this.panelSorting.Controls.Add(this.textBox8);
+            this.panelSorting.Controls.Add(this.textBox7);
+            this.panelSorting.Controls.Add(this.textBox6);
+            this.panelSorting.Controls.Add(this.textBox5);
+            this.panelSorting.Controls.Add(this.textBox4);
+            this.panelSorting.Controls.Add(this.textBox3);
+            this.panelSorting.Controls.Add(this.textBox2);
+            this.panelSorting.Controls.Add(this.textBox1);
+            this.panelSorting.Controls.Add(this.textBox0);
+            this.panelSorting.Location = new System.Drawing.Point(12, 52);
+            this.panelSorting.Name = "panelSorting";
+            this.panelSorting.Size = new System.Drawing.Size(765, 73);
+            this.panelSorting.TabIndex = 1;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(687, 13);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(70, 31);
+            this.textBox9.TabIndex = 9;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(611, 13);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(70, 31);
+            this.textBox8.TabIndex = 8;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(535, 13);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(70, 31);
+            this.textBox7.TabIndex = 7;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(459, 13);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(70, 31);
+            this.textBox6.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(383, 13);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(70, 31);
+            this.textBox5.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(307, 13);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(70, 31);
+            this.textBox4.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(231, 13);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(70, 31);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(70, 31);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox0
+            // 
+            this.textBox0.Location = new System.Drawing.Point(0, 13);
+            this.textBox0.Name = "textBox0";
+            this.textBox0.Size = new System.Drawing.Size(70, 31);
+            this.textBox0.TabIndex = 0;
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.Location = new System.Drawing.Point(12, 12);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(400, 31);
+            this.textBoxInput.TabIndex = 2;
+            this.textBoxInput.Text = "124,11,54,7,1,6,7,0,9,91";
+            // 
+            // comboBoxSpeed
+            // 
+            this.comboBoxSpeed.FormattingEnabled = true;
+            this.comboBoxSpeed.Items.AddRange(new object[] {
+            "Normal",
+            "Super Fast",
+            "Slow"});
+            this.comboBoxSpeed.Location = new System.Drawing.Point(624, 12);
+            this.comboBoxSpeed.Name = "comboBoxSpeed";
+            this.comboBoxSpeed.Size = new System.Drawing.Size(145, 33);
+            this.comboBoxSpeed.TabIndex = 4;
+            this.comboBoxSpeed.Text = "Normal";
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.AutoSize = true;
+            this.labelSpeed.Location = new System.Drawing.Point(555, 15);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(62, 25);
+            this.labelSpeed.TabIndex = 5;
+            this.labelSpeed.Text = "Speed";
+            // 
+            // FormSort
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 137);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.comboBoxSpeed);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.panelSorting);
+            this.Controls.Add(this.buttonStart);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(811, 193);
+            this.MinimumSize = new System.Drawing.Size(811, 193);
+            this.Name = "FormSort";
+            this.Text = "SelectionSort";
+            this.panelSorting.ResumeLayout(false);
+            this.panelSorting.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Panel panelSorting;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox0;
+        private System.Windows.Forms.ComboBox comboBoxSpeed;
+        private System.Windows.Forms.Label labelSpeed;
+    }
+}
+
---
+++ sortingProject/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sortingProject
+{
+    public partial class FormSort : Form
+    {
+        static int[] ParseTextToIntArr(string txt)
+        {
+            int[] numbers = new int[10];
+
+            int num = 0;
+            int position = 0;
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (txt[i] >= '0' && txt[i] <= '9')
+                {
+                    num *= 10;
+                    num += txt[i] - '0';
+                }
+                else if (txt[i] == ',')
+                {
+                    numbers[position] = num;
+                    ++position;
+                    num = 0;
+                }
+            }
+            numbers[numbers.Length - 1] = num;
+            return numbers;
+        }
+
+        public void PauseExecution(double seconds, string speed)
+        {
+            if (speed == "Super Fast")
+            {
+                seconds -= seconds * 110 / 100;
+            }
+            else if (speed == "Slow")
+            {
+                seconds += (seconds<0.1)?seconds * 10000 / 100:seconds*500/100;
+            }
+            var t = DateTime.Now;
+            DateTime t1;
+            do
+            {
+                t1 = DateTime.Now;
+            }
+            while ((double)(t1 - t).Seconds <= seconds);
+        }
+
+        public void ChangeTextBoxColor(int textBoxIndex, Color color)
+        {
+            switch (textBoxIndex)
+            {
+                case 0:
+                    textBox0.BackColor = color;
+                    break;
+                case 1:
+                    textBox1.BackColor = color;
+                    break;
+                case 2:
+                    textBox2.BackColor = color;
+                    break;
+                case 3:
+                    textBox3.BackColor = color;
+                    break;
+                case 4:
+                    textBox4.BackColor = color;
+                    break;
+                case 5:
+                    textBox5.BackColor = color;
+                    break;
+                case 6:
+                    textBox6.BackColor = color;
+                    break;
+                case 7:
+                    textBox7.BackColor = color;
+                    break;
+                case 8:
+                    textBox8.BackColor = color;
+                    break;
+                case 9:
+                    textBox9.BackColor = color;
+                    break;
+            }
+            panelSorting.Refresh();
+        }
+
+        public Color CheckTextBoxColor(int textBoxIndex)
+        {
+            Color color = Color.Gold;
+            switch (textBoxIndex)
+            {
+                case 0:
+                    color = textBox0.BackColor;
+                    break;
+                case 1:
+                    color = textBox1.BackColor;
+                    break;
+                case 2:
+                    color = textBox2.BackColor;
+                    break;
+                case 3:
+                    color = textBox3.BackColor;
+                    break;
+                case 4:
+                    color = textBox4.BackColor;
+                    break;
+                case 5:
+                    color = textBox5.BackColor;
+                    break;
+                case 6:
+                    color = textBox6.BackColor;
+                    break;
+                case 7:
+                    color = textBox7.BackColor;
+                    break;
+                case 8:
+                    color = textBox8.BackColor;
+                    break;
+                case 9:
+                    color = textBox9.BackColor;
+                    break;
+            }
+            return color;
+        }
+
+        public void CleanSortingBoard()
+        {
+            textBox1.BackColor = Color.White;
+            textBox2.BackColor = Color.White;
+            textBox3.BackColor = Color.White;
+            textBox4.BackColor = Color.White;
+            textBox5.BackColor = Color.White;
+            textBox6.BackColor = Color.White;
+            textBox7.BackColor = Color.White;
+            textBox8.BackColor = Color.White;
+            textBox0.BackColor = Color.White;
+        }
+
+        public void FillTextBoxes(int[] numbers)
+        {
+            textBox0.Text = numbers[0].ToString();
+            textBox1.Text = numbers[1].ToString();
+            textBox2.Text = numbers[2].ToString();
+            textBox3.Text = numbers[3].ToString();
+            textBox4.Text = numbers[4].ToString();
+            textBox5.Text = numbers[5].ToString();
+            textBox6.Text = numbers[6].ToString();
+            textBox7.Text = numbers[7].ToString();
+            textBox8.Text = numbers[8].ToString();
+            textBox9.Text = numbers[9].ToString();
+        }
+
+        public FormSort()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string speed = comboBoxSpeed.SelectedItem.ToString();
+            CleanSortingBoard();
+            string text = textBoxInput.Text;
+            int[] arr = ParseTextToIntArr(text);
+            FillTextBoxes(arr);
+            //start
+            for (int i = arr.Length - 1; i > 0; i--)
+            {
+                for (int k = 0; k <= 9; k++)
+                {
+                    if (CheckTextBoxColor(k) == Color.Red)
+                    {
+                        ChangeTextBoxColor(k, Color.White);
+                    }
+                }
+                ChangeTextBoxColor(arr.Length - 1, Color.Green);
+                int maxIndex = i;
+                ChangeTextBoxColor(maxIndex, Color.Blue);
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    ChangeTextBoxColor(j, Color.Yellow);
+                    PauseExecution(0.01, speed);
+                    if (arr[j] > arr[maxIndex])
+                    {
+                        if (maxIndex != i)
+                            ChangeTextBoxColor(maxIndex, Color.White);
+                        maxIndex = j;
+                        ChangeTextBoxColor(j, Color.Red);
+                        PauseExecution(0.01, speed);
+                    }
+                    if (CheckTextBoxColor(j) == Color.Yellow)
+                        ChangeTextBoxColor(j, Color.White);
+                }
+                int temp = arr[maxIndex];
+                arr[maxIndex] = arr[i];
+                arr[i] = temp;
+                ChangeTextBoxColor(i, Color.Green);
+                FillTextBoxes(arr);
+                panelSorting.Refresh();
+                PauseExecution(0.5, speed);
+            }
+            //end
+        }
+    }
+}
